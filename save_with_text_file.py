ae80969998ec69d5c4e0b49d56ed658a58192896
Fluffo <yanivpeer@gmail.com>
2024-08-16 15:15:07 +0300

    Fixed issue with folder paths (maybe was outdated code?)

--- save_with_text_file.py
+++ save_with_text_file.py
@@ -1,5 +1,5 @@
 from nodes import SaveImage
-from folder_paths import output_directory
+import folder_paths as comfy_paths
 import os
 
 class SaveWithText(SaveImage):
@@ -14,7 +14,7 @@ class SaveWithText(SaveImage):
     def func(self, images, filename_prefix, text, prompt=None, extra_pnginfo=None):
         returnable = self.save_images(images, filename_prefix, prompt, extra_pnginfo)
         for result in returnable['ui']['images']:
-            path = os.path.join(output_directory, result['subfolder'], result['filename'])
+            path = os.path.join(comfy_paths.output_directory, result['subfolder'], result['filename'])
             txt_path = os.path.splitext(path)[0] + ".txt"
             with open(txt_path, 'w') as f:
                 print(text, file=f)
